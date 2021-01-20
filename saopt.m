e6447229595cc74f58be264e511b9dbb0050d5d9
FP_kl <pang.jc@qq.com>
2021-01-20 14:49:09 +0800

    edit analysis interval

--- saopt.m
+++ saopt.m
@@ -1,15 +1,15 @@
 clear
 objfunc = @(x)obf(x);
-result = zeros(61,8);
-for sa = 15:75
+result = zeros(66,9);
+for sa = 10:75
     sa_year = sa * 1e4;
     initVal = sa_year/13 * ones(1,13);
     lb = zeros(1,13);
     ub = sa_year*ones(1,13);
     [sal_so, totalTax] = fmincon(objfunc,initVal,[],[],ones(1,13),sa_year,lb,ub);
     savedTax = ratio_compare(sa_year) - totalTax;
-    result(sa-14,:) = [sa, sal_so(1), sal_so(end), totalTax, 100*totalTax/sa_year, ... 
-    savedTax(1), savedTax(2), savedTax(3)];
+    result(sa-9,:) = [sa, sal_so(1), sal_so(end), totalTax, 100*totalTax/sa_year, ... 
+    100*sal_so(end)/sa_year, savedTax(1), savedTax(2), savedTax(3)];
 end
 
 % fprintf('Month: %.0f , Bonus: %.0f , Tax rate average = %.3f\n',sal_so(1),sal_so(end),totalTax/sa_year)

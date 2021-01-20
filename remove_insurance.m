801d617d6f120eab525f81e41696dbf31257cf36
FP_kl <pang.jc@qq.com>
2021-01-20 13:29:04 +0800

    add insurance bounds

--- remove_insurance.m
+++ remove_insurance.m
@@ -3,5 +3,12 @@ function net_salary = remove_insurance(salary)
     mr = 2;     % medical insurance & maternity insurance
     ur = 0.5;   % unemployment insurance
     hr = 7;     % hourse provident fund
+    
+    bounds = [4927, 28017];
+    if salary < bounds(1)
+        salary = bounds(1);
+    elseif salary > bounds(2)
+        salary = bounds(2);
+    end
     net_salary = salary * (1-(er+mr+ur+hr)/100);
 end
